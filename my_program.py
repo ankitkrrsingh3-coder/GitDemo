10fe7e1ef409296e5d3a27a975877c4bc95f5ff8
ankitkrrsingh3-coder <ankitkrr.singh3@gmail.com>
2026-02-14 23:08:33 +0530

    initial commit

---
+++ my_program.py
@@ -0,0 +1,14 @@
+"""
+print("welcome to python !")
+"""
+
+print("---Simple calculator---")
+
+num1_str = input("enter a number")
+num2_str = input("enter another number")
+
+num1 = int(num1_str)
+num2 = int(num2_str)
+
+sum_of_numbers = num1 + num2
+print("sum is:" , sum_of_numbers)
